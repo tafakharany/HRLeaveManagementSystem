f15a10282ac36d8234be5ec90f95e0fa428d1c4c
Taha Elfakharany <taha.ashri96@outlook.com>
2022-03-19 03:00:27 +0200

    -add: refrence to AutoMapper.
    -created MappingProfile extends the Profile  class from the AutoMapper package.

---
+++ HRLeaveManagementSystem.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HRLeaveManagementSystem.Application.DTOs;
+using HRLeaveManagementSystem.Application.DTOs.LeaveRequest;
+using HRLeaveManagementSystem.Domain;
+
+namespace HRLeaveManagementSystem.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
