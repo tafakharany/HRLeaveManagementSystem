1aabfc61a2cf78a4f1b4dd2a662f8a257210e1b0
Taha Elfakharany <taha.ashri96@outlook.com>
2022-03-19 02:58:10 +0200

    -add: DTOs that contains
    - Common that contains BaseDto.
    - Dtos for the entities we have in the domain project.

---
+++ HRLeaveManagementSystem.Application/DTOs/LeaveTypeDto.cs
@@ -0,0 +1,10 @@
+﻿using HRLeaveManagementSystem.Application.DTOs.Common;
+
+namespace HRLeaveManagementSystem.Application.DTOs
+{
+    public class LeaveTypeDto : BaseDto
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
