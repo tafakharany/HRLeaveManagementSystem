93d26d2d91c8cf8c8b72c159f169bf80dac33809
Taha Elfakharany <taha.ashri96@outlook.com>
2022-03-19 16:42:08 +0200

    -add: apply CQRS adding request and Handlers
    - LeaveType Query Handlers and Requests added

---
+++ HRLeaveManagementSystem.Application/Features/LeaveType/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HRLeaveManagementSystem.Application.DTOs;
+using HRLeaveManagementSystem.Application.Features.LeaveType.Requests.Queries;
+using HRLeaveManagementSystem.Application.Persistance.Contracts;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagementSystem.Application.Features.LeaveType.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveTyes = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDto>>(leaveTyes);
+        }
+    }
+}
---
+++ HRLeaveManagementSystem.Application/Features/LeaveType/Requests/Queries/GetLeaveTypeRequest.cs
@@ -0,0 +1,10 @@
+﻿using HRLeaveManagementSystem.Application.DTOs;
+using MediatR;
+
+namespace HRLeaveManagementSystem.Application.Features.LeaveType.Requests.Queries
+{
+    public class GetLeaveTypeRequest:IRequest<LeaveTypeDto>
+    {
+        public int Id { get; set; }
+    }
+}
