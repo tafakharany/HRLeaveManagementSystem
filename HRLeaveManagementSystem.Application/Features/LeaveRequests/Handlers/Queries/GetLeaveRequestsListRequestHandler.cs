2d8e402dacd0887bfa39a6e7b03b4baf35b0cf6c
tafakharany <taha.ashri96@outlook.com>
2022-05-20 18:41:45 +0200

    - add leaveRequest Requests and Handlers

---
+++ HRLeaveManagementSystem.Application/Features/LeaveRequests/Handlers/Queries/GetLeaveRequestsListRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using HRLeaveManagementSystem.Application.DTOs;
+using HRLeaveManagementSystem.Application.DTOs.LeaveRequest;
+using HRLeaveManagementSystem.Application.Features.LeaveAllocation.Requests.Queries;
+using HRLeaveManagementSystem.Application.Features.LeaveRequests.Requests.Queries;
+using HRLeaveManagementSystem.Application.Persistance.Contracts;
+using MediatR;
+
+namespace HRLeaveManagementSystem.Application.Features.LeaveRequests.Handlers.Queries
+{
+    public class GetLeaveRequestsListRequestHandler:IRequestHandler<GetLeaveRequestsListRequest, List<LeaveRequestDto>>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveRequestsListRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveRequestDto>> Handle(GetLeaveRequestsListRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _leaveRequestRepository.GetAll();
+            return _mapper.Map<List<LeaveRequestDto>>(result);
+        }
+    }
+}
---
+++ HRLeaveManagementSystem.Application/Features/LeaveRequests/Requests/Queries/GetLeaveRequestsListRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HRLeaveManagementSystem.Application.DTOs;
+using HRLeaveManagementSystem.Application.DTOs.LeaveRequest;
+using MediatR;
+
+namespace HRLeaveManagementSystem.Application.Features.LeaveRequests.Requests.Queries
+{
+    public class GetLeaveRequestsListRequest:IRequest<List<LeaveRequestDto>>
+    {
+        
+    }
+}
